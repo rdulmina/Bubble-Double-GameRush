9d4f08658b19dd4a38b1e820cc368a495381041b
Dulmina <rdulmina@gmail.com>
2019-11-30 11:33:09 +0530

    new version

---
+++ Assets/Scripts/welcom/display.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class display : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(randomShow());
+	}
+
+    IEnumerator randomShow()
+    {
+      
+        yield return new WaitForSeconds(2.5f);
+      
+        SceneManager.LoadScene("Homescreen");
+    }
+	
+}

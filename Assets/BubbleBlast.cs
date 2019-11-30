4cbbf4a30ba45266a9a886393a34ca27fdcf8c26
unknown <wasuradananjith@gmail.com>
2019-12-01 01:22:07 +0530

    add life bar and home background

---
+++ Assets/BubbleBlast.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleBlast : MonoBehaviour
+{
+    public Animator anim;
+    public KeyCode pops;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pops)) {
+            anim.SetTrigger("onClickPop");
+        }
+    }
+}
